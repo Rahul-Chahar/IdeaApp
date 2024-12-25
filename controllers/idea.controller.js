5ff8dc35b05311fa5cbf455dabc3815dbb9e3531
Rahul-Chahar <rahulchahar.5412381@gmail.com>
2024-12-25 15:21:10 +0530

    GET API complete

--- controllers/idea.controller.js
+++ controllers/idea.controller.js
@@ -1,7 +1,7 @@
 /**
  * Write the logic to create the controller for the idea resource.
  */
-
+const ideas = require('../models/ideas.model');
 
 
 /**
@@ -11,3 +11,11 @@
  * 
  * Return the list of all the ideas.
  */
+exports.getAllIdeas = (req, res) => {
+
+    // First read all the ideas from the idea model file
+
+    // return all those ideas
+    res.status(200).send(ideas);
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+
+// Stitching the routes here
+require('./routes/idea.routes')(app);  // This will call the function with app as an argument
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
