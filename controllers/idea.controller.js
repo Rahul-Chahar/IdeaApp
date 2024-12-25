a7947fd91758be0c7ab935f7f9c66996df381dfb
Rahul-Chahar <rahulchahar.5412381@gmail.com>
2024-12-25 14:56:33 +0530

    done with models

---
+++ controllers/idea.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Write the logic to create the controller for the idea resource.
+ */
+
+
+
+/**
+ * Create the controller for fetching all the ideas.
+ * 
+ * GET 127.0.0.1:8000/ideaApp/api/v1/ideas
+ * 
+ * Return the list of all the ideas.
+ */
---
+++ models/ideas.model.js
@@ -0,0 +1,23 @@
+module.exports ={
+    1 : {
+        id: 1,
+        idea_name : "Great Idea",
+        auther_name : "John Doe",
+        idea_description : "This is one of the finest idea I have ever come across",
+        idea_category : "Technology",
+    },
+    2 : {
+        id: 2,
+        idea_name : "Awesome Idea",
+        auther_name : "Bob Doe",
+        idea_description : "This is one of the finest idea I have ever come across",
+        idea_category : "Non-Technology",
+    },
+    3 : {
+        id: 3,
+        idea_name : "Super Idea",
+        auther_name : "Jane Doe",
+        idea_description : "This is one of the finest idea I have ever come across",
+        idea_category : "Technology",
+    },
+}
